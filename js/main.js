c30e12c1ceb910e3054fa2266f627f51188d11ec
myrdiaclonix <myrdiaclonix@pm.me>
2023-07-04 19:50:31 -0300

    feat(homepage): presentation text and sample image

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 // Atualiza o ano do footer da página.
 $("#current-year").text(new Date().getFullYear());
+
+// Referente ao Scrollspy, do Bootstrap.
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: "#navbar-example3",
+});
